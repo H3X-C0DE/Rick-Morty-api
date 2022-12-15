7bf6ee6194c99984de8b5905c02034d983e7659b
IIHEXYII <vetletessem@gmail.com>
2022-12-15 15:35:34 +0100

    added extra className

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -10,38 +10,38 @@ const Card = ({ page, results }) => {
 
       return (
         <Link style={{ textDecoration: "none" }} to={`${page}${id}`} key={id}>
-          <div className="card">
+          <div className="card card-small">
             <img src={image} alt="" />
             <div className="card-info">
               <div className="name">{name}</div>
+
+              {(() => {
+                if (status === "Dead") {
+                  return (
+                    <div>
+                      <span>Status: </span>
+                      <span className="isDead">{status}</span>
+                    </div>
+                  );
+                } else if (status === "Alive") {
+                  return (
+                    <div>
+                      <span>Status: </span>
+                      <span className="isAlive">{status}</span>
+                    </div>
+                  );
+                } else {
+                  return (
+                    <div>
+                      <span>Status: </span>
+                      <span className="isUnknown">{status}</span>
+                    </div>
+                  );
+                }
+              })()}
               <div className="">
                 <div className="">Last Location</div>
                 <div className="">{location.name}</div>
-
-                {(() => {
-                  if (status === "Dead") {
-                    return (
-                      <div>
-                        <span>Status: </span>
-                        <span className="isDead">{status}</span>
-                      </div>
-                    );
-                  } else if (status === "Alive") {
-                    return (
-                      <div>
-                        <span>Status: </span>
-                        <span className="isAlive">{status}</span>
-                      </div>
-                    );
-                  } else {
-                    return (
-                      <div>
-                        <span>Status: </span>
-                        <span className="isUnknown">{status}</span>
-                      </div>
-                    );
-                  }
-                })()}
               </div>
             </div>
           </div>
