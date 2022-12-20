b14f370b2ff02d1aa96f725c3254f8060151c709
IIHEXYII <vetletessem@gmail.com>
2022-12-20 11:45:27 +0100

    working Search function added

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,5 +1,31 @@
 import React from "react";
+/*
+takes two props as parameters - setSearch, which handles the state of the search input,
+and updatePageNumber, which updates the page number to 1 when a new search is initiated.
+*/
+const Search = ({ setSearch, updatePageNumber }) => {
+  let searchBtn = (event) => {
+    event.preventDefault();
+  };
+  /*When the text in the input element is changed, the setSearch prop is called to update the search state,
+   and the updatePageNumber prop is called to reset the page number to 1. 
+  */
+  return (
+    <form className="">
+      <input
+        onChange={(event) => {
+          updatePageNumber(1);
+          setSearch(event.target.value);
+        }}
+        placeholder="Search for characters"
+        className=""
+        type="text"
+      />
+      <button onClick={searchBtn} className="">
+        Search
+      </button>
+    </form>
+  );
+};
 
-export default function search() {
-  return <div>search</div>;
-}
+export default Search;
