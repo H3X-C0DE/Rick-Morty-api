fc9fcd0ab139b306a09b4b397d491b76f1f62cd4
IIHEXYII <vetletessem@gmail.com>
2022-12-20 14:01:35 +0100

    updated and improved code

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,21 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactPaginate from "react-paginate";
-import "../Pagination/pagination.css";
 const Pagination = ({ pageNumber, info, updatePageNumber }) => {
   let pageChange = (data) => {
     updatePageNumber(data.selected + 1);
   };
 
+  const [width, setWidth] = useState(window.innerWidth);
+  const updateDimensions = () => {
+    setWidth(window.innerWidth);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", updateDimensions);
+    return () => window.removeEventListener("resize", updateDimensions);
+  }, []);
+
   return (
     <>
       <ReactPaginate
-        className="pagination"
+        className="bg-blue-50 flex"
         nextLabel="Next"
         forcePage={pageNumber === 1 ? 0 : pageNumber - 1}
         previousLabel="Prev"
         previousClassName="btn prev"
         nextClassName="btn next"
         activeClassName="active"
+        marginPagesDisplayed={width < 576 ? 1 : 2}
+        pageRangeDisplayed={width < 576 ? 1 : 2}
         pageCount={info?.pages}
         onPageChange={pageChange}
         pageClassName="page-item"
