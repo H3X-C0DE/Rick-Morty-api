1f1a6a7e929d6ab0d83ce76f6b012d587c59c630
IIHEXYII <vetletessem@gmail.com>
2023-01-10 15:00:03 +0100

    added filter function

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,5 +1,45 @@
-import React from "react";
+import { React } from "react";
+import Gender from "./category/Gender";
+import Species from "./category/Species";
+import Status from "./category/Status";
 
-export default function filter() {
-  return <div>filter</div>;
-}
+const Filter = ({
+  // pageNumber,
+  updatePageNumber,
+  updateStatus,
+  updateGender,
+  updateSpecies,
+}) => {
+  let clear = () => {
+    updateStatus("");
+    updateGender("");
+    updateSpecies("");
+    updatePageNumber(1);
+    window.location.reload(false);
+  };
+  return (
+    <div className="filter">
+      {/* <h2 className="filter__title">Filters</h2> */}
+      <button onClick={clear} className="filter__btn">
+        Clear Filters
+      </button>
+      <div className="filter__tags" id="filterTags">
+        <Status
+          updatePageNumber={updatePageNumber}
+          updateStatus={updateStatus}
+        />
+
+        <Species
+          updatePageNumber={updatePageNumber}
+          updateSpecies={updateSpecies}
+        />
+        <Gender
+          updatePageNumber={updatePageNumber}
+          updateGender={updateGender}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Filter;
