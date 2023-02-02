7e6423deae087519f1036740814c9d6f753b5ea5
IIHEXYII <vetletessem@gmail.com>
2023-02-02 15:21:02 +0100

    fixed navbar

--- src/components/Navbar/Dropdown.jsx
+++ src/components/Navbar/Dropdown.jsx
@@ -2,31 +2,34 @@ import React from "react";
 import { useState, useRef, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 
-export default function Dropdown() {
+export default function Dropdown({ toggled }) {
   const dropdownRef = useRef(null);
-  const [isActive, setIsActive] = useState(false);
-  const onClick = () => setIsActive(!isActive);
-
+  const [isToggled, toggle] = useState(toggled);
+  const callback = () => {
+    toggle(!isToggled);
+  };
   // Closes Dropdown menu when mousedown is detected outside of menu
   useEffect(() => {
     const handleClickOutside = (event) => {
       if (!dropdownRef.current.contains(event.target)) {
-        setIsActive(false);
+        toggle(false);
       }
     };
     document.addEventListener("mousedown", handleClickOutside);
   }, [dropdownRef]);
+
   return (
-    <div className="menu-container">
-      <button onClick={onClick} className="menu-trigger">
-        <span>
-          <i className="fa-solid fa-burger"></i>
-        </span>
-      </button>
-      <nav
-        ref={dropdownRef}
-        className={`menu ${isActive ? "active" : "inactive"}`}
-      >
+    <div className="menu-container" ref={dropdownRef}>
+      <span>
+        <label htmlFor="burgerCheck" className="fa-solid fa-burger"></label>
+        <input
+          id="burgerCheck"
+          type="checkbox"
+          defaultChecked={isToggled}
+          onClick={callback}
+        />
+      </span>
+      <nav className={`menu ${isToggled ? "active" : "inactive"}`}>
         <ul>
           <li>
             <NavLink activeclassname="selected" to="/" className="navbar__link">
